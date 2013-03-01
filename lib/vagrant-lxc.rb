7e48b572d7ce96fe89ff8fb89c5f9dc296f43a8f
Fabio Rehm <fgrehm@gmail.com>
2013-03-01 20:42:56 -0300

    Add vagrant-lxc locales to I18n load path

--- lib/vagrant-lxc.rb
+++ lib/vagrant-lxc.rb
@@ -2,6 +2,8 @@ require "vagrant-lxc/version"
 
 require "vagrant-lxc/plugin"
 
+I18n.load_path << File.expand_path(File.dirname(__FILE__) + '/../locales/en.yml')
+
 module Vagrant
   module LXC
   end
