65d986c43640e3c60af7086b0921291435a46aa3
Fabio Rehm <fgrehm@gmail.com>
2013-03-04 01:14:38 -0300

    Add provisioning to the example

---
+++ example/puppet/manifests/site.pp
@@ -0,0 +1,6 @@
+Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/'] }
+
+notice "Hi there! puppet here"
+
+require hello_world
+
---
+++ example/puppet/modules/hello_world/manifests/init.pp
@@ -0,0 +1,3 @@
+class hello_world {
+  notice "Hi there! It's a puppet module here. Things seem to be working just fine ;-)"
+}
