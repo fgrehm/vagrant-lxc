a1c19371c6d12792db04a2bd6228322407ee7d8d
Fabio Rehm <fgrehm@gmail.com>
2013-04-21 18:04:36 -0300

    Prevent running acceptance specs from outside a dev machine

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
+unless ENV['USER'] == 'vagrant'
+  puts 'Acceptance specs are supposed to run from one of the vagrant dev machines'
+  exit 1
+end
+
 if defined? SimpleCov
   SimpleCov.command_name 'acceptance'
 end
