b86b83eba169b53bbc20f013de30b2f462ab448c
Fabio Rehm <fgrehm@gmail.com>
2013-11-06 21:21:16 -0200

    Fix WarnNetworks action [GH-154]

--- lib/vagrant-lxc/action/warn_networks.rb
+++ lib/vagrant-lxc/action/warn_networks.rb
@@ -7,15 +7,15 @@ module Vagrant
         end
 
         def call(env)
-          if public_or_private_network_configured?
+          if public_or_private_network_configured?(env[:machine].config)
             env[:ui].warn(I18n.t("vagrant_lxc.messages.warn_networks"))
           end
 
           @app.call(env)
         end
 
-        def public_or_private_network_configured?
-          config.vm.networks.find do |type|
+        def public_or_private_network_configured?(config)
+          config.vm.networks.find do |type, _|
             [:private_network, :public_network].include?(type.to_sym)
           end
         end
