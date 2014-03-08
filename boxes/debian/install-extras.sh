bd9b2e8957d4383ed0fecbf5c76bb33cf3b0e029
Fabio Rehm <fgrehm@gmail.com>
2014-03-08 02:32:19 -0300

    Install debian extras

--- boxes/debian/install-extras.sh
+++ boxes/debian/install-extras.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
-container_name=$1
+source common/ui.sh
 
-echo "    Will install extras for '${container_name}'"
+info 'Installing extra packages and upgrading'
+
+debug 'Bringing container up'
+lxc-start -d -n ${CONTAINER} &>/dev/null || true
+
+# TODO: Support for setting this from outside
+PACKAGES=(vim curl wget man-db bash-completion python-software-properties software-properties-common)
+
+lxc-attach -n ${CONTAINER} -- apt-get update
+lxc-attach -n ${CONTAINER} -- apt-get install ${PACKAGES[*]} -y --force-yes
+lxc-attach -n ${CONTAINER} -- apt-get upgrade -y --force-yes
