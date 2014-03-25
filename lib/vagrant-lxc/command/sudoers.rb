328477172eec264652198fa1f25ece0fc2d3e6c3
Fabio Rehm <fgrehm@gmail.com>
2014-03-24 23:50:38 -0300

    command/sudoers: Add missing `-p` to `mkdir` command

--- lib/vagrant-lxc/command/sudoers.rb
+++ lib/vagrant-lxc/command/sudoers.rb
@@ -61,7 +61,7 @@ module Vagrant
            { cmd: '/usr/bin/lxc-attach',    args: '' },
            { cmd: '/usr/bin/which',         args: 'lxc-*' },
            { cmd: '/bin/cat',               args: '/var/lib/lxc/*' },
-           { cmd: '/bin/mkdir',             args: '/var/lib/lxc/*' },
+           { cmd: '/bin/mkdir',             args: '-p /var/lib/lxc/*' },
            { cmd: '/bin/su',                args: "root -c sed -e '*' -ibak /var/lib/lxc/*" },
            { cmd: '/bin/su',                args: "root -c echo '*' >> /var/lib/lxc/*" },
            { cmd: '/usr/bin/lxc-start',     args: '-d --name *' },
