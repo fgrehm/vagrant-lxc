3257bd25bb1bd2815ef91365134664815ec1033b
Michael Stucki <michael@stucki.io>
2015-07-06 18:12:25 +0200

    Fix variable name

--- lib/vagrant-lxc/action/gc_private_network_bridges.rb
+++ lib/vagrant-lxc/action/gc_private_network_bridges.rb
@@ -35,7 +35,7 @@ module Vagrant
 
             if ! driver.bridge_is_in_use?(bridge)
               env[:ui].info I18n.t("vagrant_lxc.messages.remove_bridge", name: bridge)
-              if bridge_name != "lxcbr0"
+              if bridge != "lxcbr0"
                  driver.remove_bridge(bridge)
               end
             end
