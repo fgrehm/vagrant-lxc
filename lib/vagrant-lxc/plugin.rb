1c2208a30ee3c54cd058e9e9dfdb56a442737d15
Sergey Leschenko <sergle.ua@gmail.com>
2016-10-18 14:40:37 +0300

    Load locale file only once

--- lib/vagrant-lxc/plugin.rb
+++ lib/vagrant-lxc/plugin.rb
@@ -8,12 +8,16 @@ module Vagrant
       The LXC provider allows Vagrant to manage and control
       LXC-based virtual machines.
       EOF
+      locale_loaded = false
 
       provider(:lxc, parallel: true, priority: 7) do
         require File.expand_path("../provider", __FILE__)
 
-        I18n.load_path << File.expand_path(File.dirname(__FILE__) + '/../../locales/en.yml')
-        I18n.reload!
+        if not locale_loaded
+            I18n.load_path << File.expand_path(File.dirname(__FILE__) + '/../../locales/en.yml')
+            I18n.reload!
+            locale_loaded = true
+        end
 
         Provider
       end
